a8b8d6866306ced5f4e5358a296eb5460af388a7
nskromna <nataliaskromna@gmail.com>
2023-01-04 20:25:06 +0100

    Parametrized negative test

--- tests/test_login_page_negative.py
+++ tests/test_login_page_negative.py
@@ -8,17 +8,18 @@ class TestNegativeScenarios:
 
     @pytest.mark.login
     @pytest.mark.negative
-    def test_negative_username(self, driver):
+    @pytest.mark.parametrize("username, password, expected_error_message", [("incorrectUser", "Password123", "Your username is invalid!"), ("student", "incorrectPassword", "Your password is invalid!")])
+    def test_negative_login(self, driver, username, password, expected_error_message):
         # Open page
         driver.get("https://practicetestautomation.com/practice-test-login/")
 
         # Type username incorrectUser into Username field
         username_locator = driver.find_element(By.ID, "username")
-        username_locator.send_keys("incorrectUser")
+        username_locator.send_keys(username)
 
         # Type password Password123 into Password field
         password_locator = driver.find_element(By.ID, "password")
-        password_locator.send_keys("Password123")
+        password_locator.send_keys(password)
 
         # Puch Submit button
         submit_button_locator = driver.find_element(By.ID, "submit")
@@ -32,32 +33,4 @@ class TestNegativeScenarios:
 
         # Verify error message text is Your username is invalid!
         error_msg = error_msg_locator.text
-        assert error_msg == "Your username is invalid!", "Error message is different to expected"
-
-    @pytest.mark.login
-    @pytest.mark.negative
-    def test_negative_password(self, driver):
-        # Open page
-        driver.get("https://practicetestautomation.com/practice-test-login/")
-
-        # Type username student into Username field
-        username_locator = driver.find_element(By.ID, "username")
-        username_locator.send_keys("student")
-
-        # Type password incorrectPassword into Password field
-        password_locator = driver.find_element(By.ID, "password")
-        password_locator.send_keys("incorrectPassword")
-
-        # Puch Submit button
-        submit_button_locator = driver.find_element(By.ID, "submit")
-        submit_button_locator.click()
-        time.sleep(2)
-
-        # Verify error message is displayed
-        error_msg_locator = driver.find_element(By.ID, "error")
-        assert error_msg_locator.is_displayed(
-        ), "Error message is not displayed, but it should be"
-
-        # Verify error message text is Your password is invalid!
-        error_msg = error_msg_locator.text
-        assert error_msg == "Your password is invalid!", "Error message is different to expected"
+        assert error_msg == expected_error_message, "Error message is different to expected"
