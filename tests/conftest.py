e9278e696c18d8efd8e3845550f9102958efcaab
nskromna <nataliaskromna@gmail.com>
2023-01-04 20:26:49 +0100

    changed conftest file to serve different browsers

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,13 +3,30 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from webdriver_manager.firefox import GeckoDriverManager
 
 
 @pytest.fixture()
-def driver():
-    print("Openinig Chrome driver")
-    my_driver = webdriver.Chrome(
-        service=ChromeService(ChromeDriverManager().install()))
+def driver(request):
+    browser = request.config.getoption("--browser")
+    print(f"Openinig {browser} driver")
+
+    if browser == "chrome":
+        my_driver = webdriver.Chrome(
+            service=ChromeService(ChromeDriverManager().install()))
+    elif browser == "firefox":
+        my_driver = webdriver.Firefox(
+            service=FirefoxService(GeckoDriverManager().install()))
+    else:
+        raise TypeError(f"Expected 'chrome' or 'firefox' but got {browser}")
+         
     yield my_driver
-    print("Closing Chrome driver")
+    print(f"Closing {browser} driver")
     my_driver.quit()
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--browser", action="store", default="chrome", help="browser to execute tests (chrome or firefox)"
+    )
